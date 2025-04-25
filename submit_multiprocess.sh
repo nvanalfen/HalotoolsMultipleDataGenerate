364779ca19b822850185b3edf599ea8385f21e51
Nick Van Alfen <nvanalfen2@gmail.com>
2025-04-25 11:42:01 -0400

    Adjust script so that the max jobs from task array are automatically passed as an argument

--- submit_multiprocess.sh
+++ submit_multiprocess.sh
@@ -6,11 +6,11 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=3
-#SBATCH --array=1-4%4
+#SBATCH --array=1-5%5
 #SBATCH --output=output_logs/mp_example-%a.out
 #SBATCH --error=error_logs/mp_example-%a.err
 
 source /work/blazek_group_storage/miniconda3/bin/activate
 conda activate halotools
 
-python3 generate_median_training_data.py $SLURM_ARRAY_TASK_ID 4
+python3 generate_median_training_data.py $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
